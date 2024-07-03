740ccb0767aa891cc732bfd850046054ad7cfaa9
Mon <tracer.coolkid@gmail.com>
2024-07-03 16:37:30 +0700

    algorithm-nested-loop

--- ex-3.js
+++ ex-3.js
@@ -1,4 +1,17 @@
 // เริ่มเขียนโค้ดตรงนี้ 
-function multiplicationTable() {
-   
+
+function multiplicationTable(n) {
+    let result = "";
+    for (let i = 1; i <= n; i++) {
+        let rowResult = "";
+        for (let j = 1; j <= n; j++) {
+             rowResult += (i * j) + "\t";
+        } 
+        result += rowResult + "\n";
+    }
+    return result;
 }
+console.log(multiplicationTable(5));
+
+
+
